2c8ec818f786cec647cd703aed421d6e14d24f03
Tomas Caithaml <caitt@users.sourceforge.net>
2014-03-24 23:58:03 +0100

    Implement DB layer.
    Preliminary work. Changed API a bit, still not final version but now we
    have working implementation, though very crude and not robust.

--- src/database.hpp
+++ src/database.hpp
@@ -1,9 +1,7 @@
 /**
  * \brief Definition of database object
  *
- * Database object is a singleton providing database abstraction. Should be
- * able to absorb structures and save them into database and should be able to
- * construct objects as a response to a (search) request.
+ * Database object provides database abstraction layer.
  *
  */
 
@@ -11,33 +9,23 @@
 #define DATABASE_HPP
 
 #include <vector>
-#include <string>
-
 #include "structures.hpp"
-
-using namespace std;
 
 //! Database abstraction class
 class Database {
 public:
-   //! Singleton access method
-   static Database* get_instance() {
-      static Database* instance = NULL;
-      if(instance == NULL)
-         instance = new Database();
-      return instance;
-   }
-   //! Takes object and saves it into database
-   virtual void absorb(const File &target);
-   //! Finds files matching filter and returns them
-   virtual vector<File> get_files(
-                                  //! Maximum number of files to return
-                                  int max,
-                                  //! Filter definition
-                                  const string &filter = "");
+   
+   //! Return all files from DB
+   virtual vector<File> get_files() = 0;
+   
+   //! Changes paths of given files.
+   /// if file does not exist it created otherwise only its paths are changed
+   virtual void update_files(const vector<File>& files) = 0;
+   
+   virtual ~Database() {}
 };
 
-//! Macro for easier access
-#define DB Database::get_instance()
+//! Factory method that returns sqlite backed implementation of Database
+extern Database * open_sqlite_db(const string& filename);
 
 #endif // DATABASE_HPP
--- tests/database_test.cpp
+++ tests/database_test.cpp
@@ -1,9 +1,40 @@
+#include <stdio.h>
 #include "catch/catch.hpp"
 #include "database.hpp"
 
-TEST_CASE( "Database abstraction works", "[DB]" ) {
-   SECTION("Test singleton") {
-      Database* test = DB;
-      CHECK(test == DB);
+static vector<File> getFiles() {
+   vector<File> result = vector<File>();
+
+   result.push_back(File("tests/fixtures/dirs/a_first","dummy_storage"));
+   result.push_back(File("tests/fixtures/dirs/x_last","dummy_storage"));
+      
+   return result;
+}
+
+// so we can compare vectors of files
+bool operator==(const File& a, const File& b) {
+   return a.looks_same(b) && a.get_paths()==b.get_paths();
+}
+
+static const char * db_filename = "tomovee.sqlite";
+
+TEST_CASE("Database layer", "[DB]") {
+
+   SECTION("Empty database gives no files") {
+      remove(db_filename);
+      unique_ptr<Database> db(open_sqlite_db(db_filename));      
+      vector<File> files = db->get_files();
+      CHECK(files.size() == 0);
+   }
+
+   SECTION("DB Fillings") {
+      remove(db_filename);
+      unique_ptr<Database> db(open_sqlite_db(db_filename));
+      
+      auto oldfiles = getFiles();
+      db->update_files(oldfiles);
+      auto newfiles = db->get_files();
+      
+      CHECK(oldfiles == newfiles);
    }
 }
